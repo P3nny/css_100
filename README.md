d055e84cb2995b4bfd0b3332a60bfaef9bc587b3
p3nny <patriciaschmitter@googlemail.com>
2021-09-20 11:08:05 +0200

    ✨ Add some audio, sleep and logic

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSS 100
+
+# TODO
+
+- Panel, Zahlen untereinander anzeigen
+- Wenn eine Zahl in mehr als einem 1x1 vorkommt, dann weiteren Rand um die Number legen
+-
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function highlight(num, color) {
+let highlighted = []
 
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function highlight(num, color) {
+  console.log(highlighted);
+  let audio = new Audio(`${num}.wav`); audio.play()
   let numbers = document.getElementsByClassName("box");
   for (number of numbers) {
-    console.log(number)
-    if (number.textContent % num == 0) {
-      number.style.borderColor = `${color}`;
+    if (number.textContent % num == 0 && number.textContent <= num * 10) {
+      console.log(highlighted.includes(parseInt(number.textContent, 10)));
+      highlighted.includes(parseInt(number.textContent, 10)) ? number.style.borderColor = `orange` : number.style.borderColor = `${color}`;
+      await sleep(1700)
+      highlighted.push(parseInt(number.textContent, 10));
     }
   }
 }
